4b683252dd11fb9d6e204e5479373a15373f7829
niroe5tar64 <niroe5tar64@gmail.com>
2024-06-29 18:08:41 +0900

    [add]  Checking the operation of Panda CSS

--- src/renderer/src/components/Versions.tsx
+++ src/renderer/src/components/Versions.tsx
@@ -1,15 +1,27 @@
+import { css } from '@styled-system/css'
 import { useState } from 'react'
 
 function Versions(): JSX.Element {
-  const [versions] = useState(window.electron.process.versions)
+  const electronProcessVersionObj = window.electron?.process.versions || {
+    electron: 'ersion is unkown',
+    chrome: 'ersion is unkown',
+    node: 'ersion is unkown'
+  }
+  const [versions] = useState(electronProcessVersionObj)
 
   return (
     <ul className="versions">
       <li className="electron-version">Electron v{versions.electron}</li>
       <li className="chrome-version">Chromium v{versions.chrome}</li>
       <li className="node-version">Node v{versions.node}</li>
+      <li className={testClass}>Panda csss test</li>
     </ul>
   )
 }
 
 export default Versions
+
+const testClass = css`
+  font-size: 36px;
+  color: #fff;
+`
